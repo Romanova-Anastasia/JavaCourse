a04d017f55c74eaadd112e363c71fae71189ea3b
Romanova-Anastasia <aoonabara@gmail.com>
2022-08-11 22:52:06 +0300

    init: start HW 3_2--operators

--- src/lesson2/HW_operators.java
+++ src/lesson2/HW_operators.java
@@ -8,30 +8,60 @@ public class HW_operators {
     //в зависимости от его пола и возраста (имя, пол и возраст задаются при старте программы)
 
     public static void main (String[] args) {
-        System.out.println("Enter you name");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Enter you gender: m or w");
-        String m = "m";
-        String w = "w";
-        String gender = scanner.nextLine();
-        System.out.println("Enter you age");
-        int age = scanner.nextInt();
+//        System.out.println("Enter you name");
+//        Scanner scanner = new Scanner(System.in);
+//        String name = scanner.nextLine();
+//        System.out.println("Enter you gender: m or w");
+//        String m = "m";
+//        String w = "w";
+//        String gender = scanner.nextLine();
+//        System.out.println("Enter you age");
+//        int age = scanner.nextInt();
+//
+//        if (m.equals(gender.toLowerCase())) {
+//            if (age <= 16) {
+//                System.out.println("Hi, little boy " + name);
+//            } else {
+//                System.out.println("Hello, man " + name);
+//            }
+//        } else if (w.equals(gender.toLowerCase())) {
+//            if (age <= 16) {
+//                System.out.println("Hi, little girl " + name);
+//            } else {
+//                System.out.println("Hello, woman " + name);
+//            }
+//        } else{
+//                System.out.println("ERROR");
+//            }
 
-        if (m.equals(gender.toLowerCase())) {
-            if (age <= 16) {
-                System.out.println("Hi, little boy " + name);
-            } else {
-                System.out.println("Hello, man " + name);
-            }
-        } else if (w.equals(gender.toLowerCase())) {
-            if (age <= 16) {
-                System.out.println("Hi, little girl " + name);
-            } else {
-                System.out.println("Hello, woman " + name);
-            }
-        } else{
-                System.out.println("ERROR");
-            }
-    }
+//        Написать 5 способов создания/получения строки
+// 1. //строка
+//          String str = "Hello ";
+//          String newStr = str + "world";
+//          System.out.println(newStr); //Hello world
+
+// 2. //один фрагмент не строка, но преобразуется в строку
+//          int x = 15;
+//          String str = "Variable x = " + x;
+//          System.out.println(str); //Variable x = 15
+
+// 3. //если один из аргументов строка, то сложение будет по строковому типу
+//          int x = 15;
+//          String str = x + "20";
+//          System.out.println(str); //1520
+
+// 4. //массив строк
+//        String[] cats = {"TOM", "Sunny", "MySoul"};
+//        System.out.println(cats[0]); //TOM
+
+// 5. //пустой объект
+//        String str = new String();
+//        System.out.println(str); //пустой объект
+
+// 6. //строка через массив символов
+//        char[] chars = { 'A', 'r', 't', 'e', 'm' };
+//        String str = new String(chars);
+//        System.out.println(str);
+
+     }
 }
