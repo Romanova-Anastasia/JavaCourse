3a5934b4f0aa838355f5ab8e730cbefa696432b4
Romanova-Anastasia <aoonabara@gmail.com>
2022-08-29 20:48:49 +0300

    fix: HW_additionally Task5

--- src/HW5Array.java
+++ src/HW5Array.java
@@ -8,65 +8,54 @@ import java.util.Arrays;
 
 public class HW5Array {
 
-    public static void main (String[] args){
-        double[] myFirstArray = new double[15];
-        double[] mySecondArray = new double[15];
+    public static void main(String[] args) {
+        int[] myFirstArray = new int[15];
+        int[] mySecondArray = new int[15];
 
-        setFirstArray(myFirstArray);
-        setSecondArray(mySecondArray);
-        for(double j : myFirstArray){
+        fillArray(myFirstArray);
+        fillArray(mySecondArray);
+        for (int j : myFirstArray) {
             System.out.print(j + " ");
         }
         System.out.print("\n");
-        for(double k : mySecondArray){
+        for (int k : mySecondArray) {
             System.out.print(k + " ");
         }
         System.out.print("\n");
 
-        double sumFirst = Arrays.stream(myFirstArray).sum()/15;
-        double sumSecond = Arrays.stream(mySecondArray).sum()/15;
+        double averageFirst = findAverage(myFirstArray);
+        double averageSecond = findAverage(mySecondArray);
 
+        setAverageComparison(averageFirst, averageSecond);
+    }
+
+    public static void fillArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 5);
+        }
+    }
+
+//    public static double findAverage(int[] array) { //новый пустой массив. Называем, как хотим
+//        OptionalDouble average = Arrays.stream(array).average();
+//        System.out.print(average);
+//        if (average.isPresent()) {
+//            return average.getAsDouble();
+//        } else {
+//            return 0.0;
+//        }
+//    }
+
+    public static double findAverage(int[] array) {
+        return (double) Arrays.stream(array).sum() / array.length;
+    }
+
+    public static void setAverageComparison(double sumFirst, double sumSecond) { //новые пустые переменные. Называем, как угодно
         if (sumFirst > sumSecond) {
             System.out.print("Average first more average second: " + sumFirst + " " + sumSecond);
-        }
-        else if (sumFirst < sumSecond) {
-            System.out.print("Average first less average second: "  + sumFirst + " " + sumSecond);
-        }
-        else if (sumFirst == sumSecond) {
-            System.out.print("Average first equals average second: "  + sumFirst + " " + sumSecond);
+        } else if (sumFirst < sumSecond) {
+            System.out.print("Average first less average second: " + sumFirst + " " + sumSecond);
+        } else if (sumFirst == sumSecond) {
+            System.out.print("Average first equals average second: " + sumFirst + " " + sumSecond);
         }
     }
-
-    public static void setFirstArray(double[] myFirstArray){
-        for ( int i = 0; i < myFirstArray.length; i++){
-            myFirstArray[i] = (int) (Math.random() * 5);
-        }
-    }
-    public static void  setSecondArray(double[] mySecondArray){
-        for ( int i = 0; i < mySecondArray.length; i++){
-            mySecondArray[i] = (int) (Math.random() * 5);
-        }
-    }
-
-//    public static void setAverageFirst(int[] myFirstArray){
-//        OptionalDouble sumFirst = Arrays.stream(myFirstArray).average();
-//        System.out.print(sumFirst);
-//    }
-//
-//    public static void setAverageSecond(int[] mySecondArray){
-//        OptionalDouble sumSecond = Arrays.stream(mySecondArray).average();
-//            System.out.print(sumSecond);
-//    }
-
-//    public static void setAverageComparison (double sumSecond, double sumFirst ){
-//        if (sumFirst > sumSecond) {
-//            System.out.print("Average first more average second: " + sumFirst);
-//        }
-//        else if (sumFirst < sumSecond) {
-//            System.out.print("Average first less average second: ");
-//        }
-//        else if (sumFirst == sumSecond) {
-//            System.out.print("Average first equals average second: "  + sumFirst + " " + sumSecond);
-//        }
-//    }
 }
